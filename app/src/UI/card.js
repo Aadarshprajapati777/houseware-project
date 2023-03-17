06421f3e1034fbd70e0d00ddb3a38769acb23297
Aadarsh <aadarsh9811594152@gmail.com>
2023-03-17 17:42:38 +0530

    css added working on adding the delete icon

--- app/src/UI/card.js
+++ app/src/UI/card.js
@@ -1,11 +1,11 @@
 import React from "react"
+import "./card.css"
 
 export default function Card(props) {
    
     return(
         <div className="card_body">
             <h2 className="card">{props.value}</h2>
-            
         </div>
     );
 };
--- app/src/components/screen2.js
+++ app/src/components/screen2.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Card from "../UI/card";
+import "./screen2.css";
 import { inputvalue } from "../components/inputfield";
 
 export default function Screen2() {
     const characters = inputvalue.split(""); 
     
     return (
-        <div className="character">
+        <div className="character_container">
             {characters.map((character) => (
-                <Card key={character} value={character} />
+                <Card key={character} value={character} className="character_card" />
             ))}
+            
+
         </div>
     );
 }
